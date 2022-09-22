d5b1fd24c717bb3e226eb274635907c137ebac92
mlkbly <balayimalek.com>
2022-09-22 14:33:54 +0330

    phase 3.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from .models import Post
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+def creat1000(request):
+    me=User.objects.get(username='admin')
+    for i in range(100):
+        Post.objects.create(author=me, title=f'Sample title{i}', text=f'Test{i}')
+    return HttpResponse("sss")
